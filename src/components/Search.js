6ac35c108d5ac42ac4da093b9a14406a50ee3a1b
Michelle <nyamburamichelle400@gmail.com>
2024-04-28 23:50:01 +0300

    does challenge

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Search() {
+function Search({ transactions}) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  //const handleChange = (e) => {
+    //const query = e.target.value;
+    //setSearchQuery(query);
+
+    const filtered = transactions.filter((transaction) =>
+      transaction.description
+    );
+
+    setFilteredTransactions(filtered);
+  ;
+
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder="Search your Recent Transactions"
-        onChange={() => console.log("Searching...")}
+        value={searchQuery}
+        onChange={handleChange}
       />
       <i className="circular search link icon"></i>
     </div>
   );
-}
 
+}
 export default Search;
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Transaction() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions")
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setTransactions(data);
+      });
+  }, []);
+
   return (
-    <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-    </tr>
+    <>
+      {transactions.map((oneTransaction) => (
+        <tr key={oneTransaction.id}>
+          <td>{oneTransaction.date}</td>
+          <td>{oneTransaction.description}</td>
+          <td>{oneTransaction.category}</td>
+          <td>{oneTransaction.amount}</td>
+        </tr>
+      ))}
+    </>
   );
 }
 
